ae722b200d7d42083b308b6db435a4f03636ae88
BorjaGJ <borjagj25@gmail.com>
2022-09-17 17:28:59 +0200

    host

--- PokeRandom/settings.py
+++ PokeRandom/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2cg7bkww0uygjmi$kwcwvfne_sfyx-#j-&w*nm((q0c8dv1#0u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["borji.pythonanywhere.com"]
 
 
 # Application definition
